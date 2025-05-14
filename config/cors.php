07d828218a8e7e73f387d2efb85891b276315f83
barry white <Barracksila@gmail.com>
2025-05-14 23:42:38 +0300

    Initial commit

--- config/cors.php
+++ config/cors.php
@@ -12,8 +12,11 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'], // Define which routes are allowed to be accessed
 
     'allowed_methods' => ['*'], // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-
-    'allowed_origins' => ['http://localhost:8080'], // Allow your frontend's URL (update this if needed)
+'allowed_origins' => [
+    'http://localhost:8080',
+    'http://localhost:5173',
+],
+ // Allow your frontend's URL (update this if needed)
 
     'allowed_origins_patterns' => ['/^https?:\/\/(localhost|yourdomain)\.com$/'], // Allow specific patterns, if needed
 
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('api')->get('/example', function (Request $request) {
     return response()->json(['message' => 'Hello, world!']);
 });
+
+
+
+
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello from Laravel']);
+});
