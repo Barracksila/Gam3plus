3ff535ab545057a636cb5ece53ac12093ab33861
barry white <Barracksila@gmail.com>
2025-06-25 14:34:45 +0300

    Initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,3 @@
 import './bootstrap';
 
 
-// For Vue 3
-import { createApp } from 'vue';
-import ExampleComponent from './components/ExampleComponent.vue';
-
-createApp({
-    components: {
-        ExampleComponent
-    }
-}).mount('#app');
